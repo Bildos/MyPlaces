3e7fe53edfd501a152cc45b201522a41bbcece5d
Andrey Borisow <giggsololo@gmail.com>
2019-04-04 21:56:12 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Андрей on 04.04.2019.
+//  Copyright © 2019 Solo Mid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
