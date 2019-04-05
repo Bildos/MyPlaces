4f0fd72f35c0bba72bdbf46b6b98311feb885263
Andrey <giggsololo@gmail.com>
2019-04-05 23:03:40 +0300

    Круглые картинки + увеличенные строки

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -31,10 +31,15 @@ let restarauntNames = ["Burger King", "KFC", "Golden Cofe", "Paragraph", "Burger
 
         cell.textLabel?.text = restarauntNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restarauntNames[indexPath.row])
-
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
     
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     /*
     // MARK: - Navigation
 
