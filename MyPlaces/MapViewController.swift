f402aa2837a71fc3ce08412ee78d1be2e0749c5c
Andrey <giggsololo@gmail.com>
2019-04-27 09:23:52 +0300

    Добавил новый переход на MapView с текущим местоположением пользователя

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -16,30 +16,39 @@ class MapViewController: UIViewController {
     let annotationIdentifier = "annotationIdentifier"
     let locationManager = CLLocationManager()
     let regionInMeters = 10_000.00
+    var incomeSegueIdentifier = ""
     
     @IBOutlet var mapView: MKMapView!
-    
+    @IBOutlet var mapPinImage: UIImageView!
+    @IBOutlet var currentAdres: UILabel!
+    @IBOutlet var doneButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        setupPlacemark()
+        setupMapView()
         checkLocationServices()
     }
     
     @IBAction func centerViewInUserLocation() {
-        
-        if let location = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion(center: location,
-                                            latitudinalMeters: regionInMeters,
-                                            longitudinalMeters: regionInMeters)
-            mapView.setRegion(region, animated: true)
-        }
+        showUserController()
+    }
+    
+    @IBAction func doneButtonPressed() {
     }
     
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
-    
+    private func setupMapView() {
+        if incomeSegueIdentifier == "showPlace" {
+            setupPlacemark()
+            mapPinImage.isHidden = true
+            currentAdres.isHidden = true
+            doneButton.isHidden = true
+        }
+        
+        
+    }
     private func setupPlacemark() {
         
         guard let location = place.location else { return }
@@ -93,6 +102,8 @@ class MapViewController: UIViewController {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "getAdress" { showUserController() }
+            
             break
         case .denied:
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
@@ -111,6 +122,17 @@ class MapViewController: UIViewController {
         @unknown default:
             print("New case is available")
         }
+    }
+    
+    private func showUserController() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+        
     }
     
     private func showAlert(title: String, message: String) {
